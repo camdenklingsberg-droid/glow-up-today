b3dc7dd82cd545070b2f4e98a4826a4daf015426
camdenklingsberg-droid <camden.klingsberg@saintandrews.net>
2026-06-11 22:09:22 -0400

    Create config.py

---
+++ app/config.py
@@ -0,0 +1,16 @@
+import os
+
+class Config:
+    SECRET_KEY = os.environ.get("SECRET_KEY", "dev-key")
+
+    SQLALCHEMY_DATABASE_URI = "sqlite:///glowup.db"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    STRIPE_SECRET_KEY = os.environ.get("STRIPE_SECRET_KEY")
+    STRIPE_PUBLIC_KEY = os.environ.get("STRIPE_PUBLIC_KEY")
+
+    MAIL_SERVER = "smtp.gmail.com"
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
